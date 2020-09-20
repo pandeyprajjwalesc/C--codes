dbf18f8b3b296b363f5c319aa2dcc4c6ff15831a
Prajjwal Pnadey <71452345+pandeyprajjwalesc@users.noreply.github.com>
2020-09-20 23:30:23 +0530

    Add files via upload

---
+++ ASCII VALUE.C.c
@@ -0,0 +1,8 @@
+//ASCII VALUE OF A CHARACTER.
+#include<stdio.h>
+int main ()
+{ char X;
+printf("Enter a character here:");
+scanf("%c",&X);
+printf("The ASCII VALUE of entered character is %d.",X);
+}
